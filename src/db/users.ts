2583971bec70657542cfdd8c5067411dfdd29468
Sphericalkat <me@kat.bio>
2024-11-17 23:12:04 +0530

    feat(home): fetch data from chatwork api with auth middleware
    Signed-off-by: Sphericalkat <me@kat.bio>

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,39 +1,36 @@
-import { eq } from "drizzle-orm";
-import { db } from ".";
-import { sessionTable, userTable } from "./schema";
-import { User } from "@/lib/types";
+import { eq } from 'drizzle-orm';
+import { db } from '.';
+import { sessionTable, userTable } from './schema';
+import { User } from '@/lib/types';
 
 export async function getUserByChatworkId(id: string): Promise<User | null> {
-  const result = await db
-    .select()
-    .from(userTable)
-    .where(eq(userTable.chatworkId, id));
-  if (result.length < 1) {
-    return null;
-  }
+	const result = await db.select().from(userTable).where(eq(userTable.chatworkId, id));
+	if (result.length < 1) {
+		return null;
+	}
 
-  return result[0];
+	return result[0];
 }
 
 export async function createUser(chatworkId: string): Promise<User> {
-  const user = {
-    chatworkId,
-  };
+	const user = {
+		chatworkId
+	};
 
-  const inserted = await db.insert(userTable).values(user).returning();
-  return inserted[0];
+	const inserted = await db.insert(userTable).values(user).returning();
+	return inserted[0];
 }
 
 export async function updateUserTokens(
-  id: number,
-  accessToken: string,
-  refreshToken: string
+	id: number,
+	accessToken: string,
+	refreshToken: string
 ): Promise<void> {
-  await db
-    .update(sessionTable)
-    .set({
-      accessToken,
-      refreshToken,
-    })
-    .where(eq(sessionTable.userId, id));
+	await db
+		.update(sessionTable)
+		.set({
+			accessToken,
+			refreshToken
+		})
+		.where(eq(sessionTable.userId, id));
 }
--- src/lib/chatwork/index.ts
+++ src/lib/chatwork/index.ts
@@ -4,8 +4,24 @@ export * from "./runtime";
 export * from "./apis/index";
 export * from "./models/index";
 
+import { getCurrentSession } from "../cookies";
 import { DefaultApi } from "./apis/DefaultApi";
-import { Configuration } from "./runtime";
+import { Configuration, FetchParams, RequestContext } from "./runtime";
 
 const config = new Configuration();
 export const api = new DefaultApi(config);
+
+const authMiddleware = async (
+  context: RequestContext
+): Promise<FetchParams | void> => {
+  const { session } = await getCurrentSession();
+  if (session) {
+    context.init.headers = {
+      ...context.init.headers,
+      Authorization: `Bearer ${session.accessToken}`,
+    };
+  }
+  return Promise.resolve(context);
+};
+
+export const authApi = new DefaultApi(config).withPreMiddleware(authMiddleware);
